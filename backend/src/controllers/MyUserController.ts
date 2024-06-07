e0d7514614fd40651ac54ed9835fa86279a07c24
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-06-07 18:03:31 +0530

    Get user API

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,20 +1,35 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req:Request,res:Response)=>{
+  try {
+
+    const currentUser = await User.findOne({_id:req.userId})
+
+    if(!currentUser){
+      return res.status(404).json({message:"User not fount"})
+    }
+
+    res.json(currentUser)
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({message:"Something went wrong"})
+    
+  }
+}
+
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.body;
     const exsistingUser = await User.findOne({ auth0Id });
 
     if (exsistingUser) {
-      return res.status(200).json({message:"User already exsists"})
+      return res.status(200).json({ message: "User already exsists" });
     }
 
     const newUser = new User(req.body);
     await newUser.save();
-
-    
-    
 
     res.status(201).json(newUser.toObject());
   } catch (error) {
@@ -23,30 +38,31 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
-const updateCurrentUser = async (req:Request,res:Response)=>{
+const updateCurrentUser = async (req: Request, res: Response) => {
   try {
-    const {name,addressLine1 , country,city} = req.body
-    const user = await User.findById(req.userId)
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
 
-    if(!user){
-      return res.status(404).json({message:"User not found"})
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    user.name = name
-    user.addressLine1 = addressLine1
-    user.country = country
-    user.city = city
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.country = country;
+    user.city = city;
 
-    await user.save()
+    await user.save();
 
-    res.send(user )
-
+    res.send(user);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({message:"Error updating user"})
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
   }
-}
+};
 
 export default {
-  createCurrentUser,updateCurrentUser,
+  getCurrentUser,
+  createCurrentUser,
+  updateCurrentUser,
 };
