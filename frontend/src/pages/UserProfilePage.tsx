e5c37bec4c6f958081a611d487f47c626a5eeb34
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-06-07 14:53:14 +0530

    User profile form

---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,10 @@
+import userProfileForm from "../forms/user-profile-form/UserProfileForm";
+const UserProfilePage = () => {
+  return(
+    <>
+    <userProfileForm />
+    </>
+  )
+}
+
+export default UserProfilePage;
