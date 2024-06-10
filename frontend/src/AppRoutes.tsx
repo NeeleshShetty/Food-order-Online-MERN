707700926a15914c1a419e75a6bfddbff7e0110e
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-06-10 17:49:56 +0530

    create restaurant form

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
 const AppRoutes = () => {
   return (
@@ -28,9 +29,17 @@ const AppRoutes = () => {
             </Layout>
           }
         />
+         <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
       </Route>
 
-      <Route path="*" element={<Navigate to="/" />} />
+      {/* <Route path="*" element={<Navigate to="/" />} /> */}
     </Routes>
   );
 };
--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -7,6 +7,7 @@ const MobileNavLinks = () => {
   return(
     <>
     <Link to={'/user-profile'} className="flex bg-white items-center font-bold hover:text-orange-500 "> User Profile</Link>
+    <Link to={'/manage-restaurant'} className="flex bg-white items-center font-bold hover:text-orange-500 "> Manage Restaurant</Link>
     <Button className="flex items-center px-3 font-bold hover:bg-gray-500" onClick={()=> logout()}>
             Log Out
     </Button>
