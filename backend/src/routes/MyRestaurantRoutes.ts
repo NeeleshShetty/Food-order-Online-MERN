15e8150454b7a39ba5075840307bda3ed06ce565
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-06-11 19:55:29 +0530

    Update my restaurant API

--- backend/src/routes/MyRestaurantRoutes.ts
+++ backend/src/routes/MyRestaurantRoutes.ts
@@ -27,4 +27,11 @@ router.post(
   MyRestaurantController.createMyRestaurant
 );
 
+router.put("/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant)
+
 export default router;
