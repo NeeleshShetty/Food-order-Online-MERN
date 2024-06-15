b336d065cd578d82f1f59ff1f489d5faa13bdf98
NeeleshBShetty <neeleshshetty@1236@gmail.com>
2024-06-15 14:09:51 +0530

    Search Page API req

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,30 @@
 import { useParams } from "react-router-dom";
+import { useSearchRestaurants} from "../api/RestaurantApi";
 
 const SearchPage = () => {
   const {city} = useParams()
+  const {results,isLoading} = useSearchRestaurants(city)
 
+  if (isLoading) {
+    return <p>Loading restaurants...</p>;
+  }
+
+  if (!results) {
+    return <p>Error fetching restaurants</p>; // Handle potential errors
+  }
+
+    
   return(
     <span>
-        {city}2342342
+        {city}
+     
+       <div>
+        {results?.data.map((res)=>(
+          <h1>
+            {res.restaurantName}
+          </h1>
+        ))}
+       </div>
     </span>
   )
 }
